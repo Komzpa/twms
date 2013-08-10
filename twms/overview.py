d35e5030f1352eb12091833306df3795988b45df
Andrew Shadura <andrew@shadura.me>
2013-08-10 19:14:04 +0200

    Fix HTML template.

--- twms/overview.py
+++ twms/overview.py
@@ -22,7 +22,8 @@ def html(ref):
     """
     Gives overall information about tWMS server and its layers in HTML format.
     """
-    resp = "<html><head><title>"
+    resp = "<!doctype html>"
+    resp += "<html><head><title>"
     resp += wms_name
     resp += "</title></head><body><h2>"
     resp += wms_name
@@ -30,12 +31,12 @@ def html(ref):
     for i in layers:
       bbox =  layers[i].get("data_bounding_box", projections.projs[layers[i]["proj"]]["bounds"] )
       resp += "<tr><td><img src=\""
-      resp += ref + "?layers=" + i+ "&bbox=%s,%s,%s,%s" % bbox + "&width=200\" width=\"200\" /></td><td><h3>"
+      resp += ref + "?layers=" + i+ "&amp;bbox=%s,%s,%s,%s" % bbox + "&amp;width=200&amp;format=image/png\" width=\"200\" /></td><td><h3>"
       resp += layers[i]["name"]
-      resp += "</h3><b>Bounding box:</b> "+ str(bbox) +" (show on <a href=\"http://openstreetmap.org/?minlon=%s&minlat=%s&maxlon=%s&maxlat=%s&box=yes\">OSM</a>" % bbox  +")<br />"
+      resp += "</h3><b>Bounding box:</b> "+ str(bbox) +" (show on <a href=\"http://openstreetmap.org/?minlon=%s&amp;minlat=%s&amp;maxlon=%s&amp;maxlat=%s&amp;box=yes\">OSM</a>" % bbox  +")<br />"
       resp += "<b>Projection:</b> "+ layers[i]["proj"]  +"<br />"
-      resp += "<b>WMS half-link:</b> "+ ref+"?layers="+ i +"&<br />"
-      resp += "<b>Tiles URL:</b> "+ ref+"?layers="+ i +"&request=GetTile&z=!&x=!&y=! <br />"
+      resp += "<b>WMS half-link:</b> "+ ref+"?layers="+ i +"&amp;<br />"
+      resp += "<b>Tiles URL:</b> "+ ref+"?layers="+ i +"&amp;request=GetTile&amp;z=!&amp;=!&amp;y=! <br />"
       resp += "</td></tr>"
     resp += "</table></body></html>"
     return resp
